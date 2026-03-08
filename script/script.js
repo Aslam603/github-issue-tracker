814e1e47879c730e0b0220998f9df0e1ec12c67b
Shelby1920 <aslamanik28@gmail.com>
2026-03-08 19:34:25 +0600

    worked on buttons & issue section.

---
+++ script/script.js
@@ -0,0 +1,17 @@
+document.getElementById("login-btn").addEventListener("click",function(){
+
+    const userName=document.getElementById("userNaam");
+        const userId=userName.value ;
+
+ const userPassword=document.getElementById("userPass")
+ const userPasses = userPassword.value;
+
+if(userId=="admin"  && userPasses=="admin123"){
+    // alert("login success")
+    window.location.assign("/main.html")
+} else{
+    // alert("try again, info is not valid!!")
+    return;
+}
+
+});
